427f8eff6afa64c5d9ca595f030153e208d1379a
Николай <pakhomovnikolay@live.com>
2022-11-18 08:23:37 +0300

    Добавлен ресурс со списком кистей сообщений

---
+++ Project_Сonfigurator/Resources/BrushList/BrushMessage/BrushMessage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace Project_Сonfigurator.Resources.BrushList.BrushMessage
+{
+    public class BrushMessage : IBrushMessage
+    {
+        #region Список кистей для сообщения
+        /// <summary>
+        /// Список кистей для сообщения
+        /// </summary>
+        public List<SolidColorBrush> BrushList { get; set; } = new() { Brushes.Transparent, Brushes.Red, Brushes.Yellow, Brushes.Green };
+        #endregion
+    }
+}
---
+++ Project_Сonfigurator/Resources/BrushList/BrushMessage/IBrushMessage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace Project_Сonfigurator.Resources.BrushList.BrushMessage
+{
+    public interface IBrushMessage
+    {
+        #region Список кистей для сообщения
+        /// <summary>
+        /// Список кистей для сообщения
+        /// </summary>
+        List<SolidColorBrush> BrushList { get; set; }
+        #endregion
+
+    }
+}
