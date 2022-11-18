4a8fc89b5f453f026507339f2871689c0f64e25a
Николай <pakhomovnikolay@live.com>
2022-11-18 08:30:49 +0300

    Добавлен ресурс ковертера кистей

---
+++ Project_Сonfigurator/Infrastructures/Converters/BrushConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Project_Сonfigurator.Infrastructures.Converters
+{
+    public class BrushConverter : IValueConverter
+    {
+        public object Convert(object v, Type t, object p, CultureInfo c)
+        {
+            if (v is not Color brush) return null;
+            return new SolidColorBrush(brush);
+        }
+
+        public object ConvertBack(object v, Type t, object p, CultureInfo c)
+        {
+            throw new NotImplementedException("Обратное преобразование не поддерживается");
+        }
+    }
+}
