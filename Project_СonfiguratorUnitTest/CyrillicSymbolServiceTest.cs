1c69e51673f5257964ce7b9183e480f24d977812
Николай <pakhomovnikolay@live.com>
2023-02-10 17:22:29 +0300

    Добавлены тестовые модули для сервиса проверки наличия русских символов

---
+++ Project_СonfiguratorUnitTest/CyrillicSymbolServiceTest.cs
@@ -0,0 +1,56 @@
+using Project_Сonfigurator.Services;
+
+namespace Project_СonfiguratorUnitTest
+{
+    [TestClass]
+    public class CyrillicSymbolServiceTest
+    {
+        [TestMethod]
+        public void IsCyrillicSymbolTest_1()
+        {
+            bool expected = true;
+            CyrillicSymbolService Service = new();
+            Assert.AreEqual(expected, !Service.CheckAvailability("123"), "CyrillicSymbolService - Тест не пройден");
+        }
+
+        [TestMethod]
+        public void IsCyrillicSymbolTest_2()
+        {
+            bool expected = true;
+            CyrillicSymbolService Service = new();
+            Assert.AreEqual(expected, !Service.CheckAvailability("qwe"), "CyrillicSymbolService - Тест не пройден");
+        }
+
+        [TestMethod]
+        public void IsCyrillicSymbolTest_3()
+        {
+            bool expected = true;
+            CyrillicSymbolService Service = new();
+            Assert.AreEqual(expected, Service.CheckAvailability("123йцу"), "CyrillicSymbolService - Тест не пройден");
+        }
+
+        [TestMethod]
+        public void IsCyrillicSymbolTest_4()
+        {
+            bool expected = true;
+            CyrillicSymbolService Service = new();
+            Assert.AreEqual(expected, Service.CheckAvailability("йцу123"), "CyrillicSymbolService - Тест не пройден");
+        }
+
+        [TestMethod]
+        public void IsCyrillicSymbolTest_5()
+        {
+            bool expected = true;
+            CyrillicSymbolService Service = new();
+            Assert.AreEqual(expected, Service.CheckAvailability("qweйцу"), "CyrillicSymbolService - Тест не пройден");
+        }
+
+        [TestMethod]
+        public void IsCyrillicSymbolTest_6()
+        {
+            bool expected = true;
+            CyrillicSymbolService Service = new();
+            Assert.AreEqual(expected, Service.CheckAvailability("йцу"), "CyrillicSymbolService - Тест не пройден");
+        }
+    }
+}
